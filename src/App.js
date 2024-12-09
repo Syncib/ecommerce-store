22b0b66c9eb84a84ecd3430fea1640a7e3fe4193
Syncib <saqibabbas@live.co.uk>
2024-12-09 23:45:18 +0500

    `Updated conditional rendering for AdminPanel component in App.js`

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
           <Route
             path="/dashboard"
             element={
-              user?.role === "admin" ? (
+              user?.role == "admin" ? (
                 <AdminPanel />
               ) : (
                 <Navigate to={"/"} />
