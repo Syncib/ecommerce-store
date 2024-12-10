f199d53de396f2623a356c4b4ecf2029ea46a600
Syncib <saqibabbas@live.co.uk>
2024-12-11 00:42:16 +0500

    `Refactor CSS: Update styles for admin coupons and modify items`

---
+++ src/pages/ModifyItems.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ModifyItems = () => {
+  return (
+    <div className='modify-items'>
+        <h1>Modify Items</h1>
+        <div className='item-list'>
+            <div className='item'>
+                <img />
+                <div className='item-name'></div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ModifyItems
